80f1befb5dc0b21e6c998ef25de033141f29e353
Samuel ODaniels <samodan195@gmail.com>
2022-07-23 00:35:49 +0100

    Write test for Player#set_symbol

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,17 @@
 #frozen_string_literal: true
+
 require_relative '../lib/player'
 
 RSpec.describe Player do
-  
+  subject(:player) { described_class.new }
+
+  describe '#set_symbol' do
+    context 'when argument is a string' do
+      it 'changes @symbol to passed in symbol' do
+        sym_arg = '\u2648'
+        player.set_symbol(sym_arg)
+        expect(player.symbol).to eq(sym_arg)
+      end
+    end
+  end
 end
