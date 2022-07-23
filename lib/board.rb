8cd133ade2fcbf8dd5ab2eb6845057f3d9765012
Samuel ODaniels <samodan195@gmail.com>
2022-07-23 20:30:28 +0100

    Write Board#found_four?

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,17 @@ class Board
   def initialize
     @grid = Array.new(42)
   end
+
+  def found_four?(arr)
+    choice = "\u2648"
+    count = 0
+    arr.each_with_index do |item, idx|
+      if item == choice && arr.at(idx + 1) == choice
+        count += 1
+      end
+      count = 0 if item != choice
+      return true if count == 3
+    end
+    false
+  end
 end
