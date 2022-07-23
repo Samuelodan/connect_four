9b2f66b37f9e4a259b93b337e08597e836e8cff2
Samuel ODaniels <samodan195@gmail.com>
2022-07-23 20:34:12 +0100

    Refactor Board#found_four? to pass updated tests

--- lib/board.rb
+++ lib/board.rb
@@ -7,8 +7,7 @@ class Board
     @grid = Array.new(42)
   end
 
-  def found_four?(arr)
-    choice = "\u2648"
+  def found_four?(choice, arr)
     count = 0
     arr.each_with_index do |item, idx|
       if item == choice && arr.at(idx + 1) == choice
