f677951882e5e1dc35fb59dcda0e46b41d5f37c6
Samuel ODaniels <samodan195@gmail.com>
2022-07-23 20:18:03 +0100

    Redesign test examples for Board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,31 +7,19 @@ RSpec.describe Board do
 
   describe '#found_four?' do
     context 'when there are 4 consecutive symbols' do
-      before do
-        symbol = "\u2648"
-        board.grid[8],
-        board.grid[16],
-        board.grid[24],
-        board.grid[32] = Array.new(4, symbol)
-      end
-
       it 'returns true' do
-        result = board.found_four?
+        symbol = "\u2648"
+        line = ['bread', symbol, symbol, symbol, symbol, 'john']
+        result = board.found_four?(line)
         expect(result).to be(true)
       end
     end
 
     context 'when there are less than 4 consecutive symbols' do
-      before do
-        symbol = "\u2648"
-        board.grid[8],
-        board.grid[15],
-        board.grid[24],
-        board.grid[18] = Array.new(4, symbol)
-      end
-
       it 'returns false' do
-        result = board.found_four?
+        symbol = "\u2648"
+        line = ['bread', symbol, symbol, 'judas', symbol, symbol]
+        result = board.found_four?(line)
         expect(result).to be(false)
       end
     end
