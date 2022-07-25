e6722e15578d272bf6ea6fe970c16a50400dd9c5
Samuel ODaniels <samodan195@gmail.com>
2022-07-25 14:32:12 +0100

    Update symbol in Board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Board do
   describe '#found_four?' do
     context 'when there are 4 consecutive symbols' do
       it 'returns true' do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         line = ['bread', symbol, symbol, symbol, symbol, 'john']
         result = board.found_four?(symbol ,line)
         expect(result).to be(true)
@@ -17,7 +17,7 @@ RSpec.describe Board do
 
     context 'when there are less than 4 consecutive symbols' do
       it 'returns false' do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         line = ['bread', symbol, symbol, 'judas', symbol, symbol]
         result = board.found_four?(symbol, line)
         expect(result).to be(false)
@@ -28,14 +28,14 @@ RSpec.describe Board do
   describe '#drop' do
     context 'when column 1 is empty' do
       it 'drops item into the grid' do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         column_no = 1
         board.drop(count: column_no, symbol: symbol)
         expect(board.grid[35]).to eq(symbol)
       end
 
       it 'second slot is nil' do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         column_no = 1
         board.drop(count: column_no, symbol: symbol)
         expect(board.grid[28]).to be(nil)
@@ -44,20 +44,20 @@ RSpec.describe Board do
 
     context 'when column 1 has 4 symbols' do
       before do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         board.grid[35], board.grid[28],
         board.grid[21], board.grid[14] = Array.new(4, symbol)
       end
 
       it 'drops item into the grid' do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         column_no = 1
         board.drop(count: column_no, symbol: symbol)
         expect(board.grid[7]).to eq(symbol)
       end
 
       it 'top slot is empty' do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         column_no = 1
         board.drop(count: column_no, symbol: symbol)
         expect(board.grid[0]).to be(nil)
@@ -68,7 +68,7 @@ RSpec.describe Board do
   describe '#column_full?' do
     context 'when column is not full' do
       before do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         board.grid[36], board.grid[29],
         board.grid[22], board.grid[15] = Array.new(4, symbol)
       end
@@ -81,13 +81,13 @@ RSpec.describe Board do
 
     context 'when column is full' do
       before do
-        symbol = "\u2648"
+        symbol = "\e[91m\u25CF\e[0m"
         board.grid[36], board.grid[29],
         board.grid[22], board.grid[15],
         board.grid[8], board.grid[1] = Array.new(6, symbol)
       end
 
-      xit 'returns true' do
+      it 'returns true' do
         result = board.column_full?(column: 2)
         expect(result).to be(true)
       end
