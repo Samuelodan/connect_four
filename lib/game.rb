01a6af6182834c2d694cbfd37405b230d3ade2ae
Samuel ODaniels <samodan195@gmail.com>
2022-07-26 20:55:42 +0100

    Write Game#assign_symbol

--- lib/game.rb
+++ lib/game.rb
@@ -8,4 +8,11 @@ class Game
     @player1 = player1
     @player2 = player2
   end
+
+  def assign_symbol
+    symbol_for_p1 = "\e[91m\u25CF\e[0m"
+    symbol_for_p2 = "\e[37m\u25CF\e[0m"
+    player1.set_symbol(symbol_for_p1)
+    player2.set_symbol(symbol_for_p2)
+  end
 end
