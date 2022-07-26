b0af98a35559f7c62610e75b34f4bf5c96bf772d
Samuel ODaniels <samodan195@gmail.com>
2022-07-26 20:45:00 +0100

    Create Game class

---
+++ lib/game.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Game
+  attr_reader :board, :player1, :player2
+
+  def initialize(board, player1, player2)
+    @board = board
+    @player1 = player1
+    @player2 = player2
+  end
+end
