c893209a6f8c9d173df7cbe1ddc753a4192552fa
Samuel ODaniels <samodan195@gmail.com>
2022-07-26 20:58:24 +0100

    Fix typo in test context

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Game do
   end
 
   describe '#assign_symbol' do
-    context 'for player1' do
+    context 'for player2' do
       it "changes player2's symbol from nil" do
         desired_symbol = "\e[37m\u25CF\e[0m"
         expect { game.assign_symbol }.to change { game.player2.symbol }.from(nil).to(desired_symbol)
