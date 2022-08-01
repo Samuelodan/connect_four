94402ccf6c335252b332d9e52eaacc43197907c2
Samuel ODaniels <samodan195@gmail.com>
2022-08-01 15:36:55 +0100

    Move text-heavy Game methods to module Display

--- lib/display.rb
+++ lib/display.rb
@@ -1,5 +1,51 @@
 # frozen_string_literal: true
 
 module Display
-  
+  def introduce
+    system('clear')
+    puts <<~HEREDOC
+
+
+      W E L C O M E  TO  C O N N E C T  F O U R
+
+      YOU WIN THIS GAME BY BEING THE FIRST TO FORM AN UNBROKEN DIAGONAL,
+      VERTICAL, OR HORIZONTAL LINE WITH 4 OF YOUR PIECES.
+      YOU FORM THESE LINES BY DROPPING YOUR PIECES STRAIGHT INTO ANY ONE
+      OF THE 7 COLUMNS.
+      
+      ALRIGHT, THAT SAID, LET THE GAME BEGIN!
+
+      enter any key to continue...\n
+    HEREDOC
+    print '>> '
+    gets.chomp
+  end
+
+  def display_player_sym
+    puts <<-HEREDOC
+    
+    #{player1.name} is #{player1.symbol} :: #{player2.name} is #{player2.symbol}
+      
+    HEREDOC
+  end
+
+  def declare_win(name)
+    puts <<~HEREDOC
+      **************************************
+      YAY!! #{name} won this round.
+      Good game.
+
+    HEREDOC
+  end
+
+  def declare_tie
+    puts <<~HEREDOC
+
+    G A M E O V E R!
+
+    That was one tough match for sure.
+    Match ended in a tie.
+
+    HEREDOC
+  end
 end
